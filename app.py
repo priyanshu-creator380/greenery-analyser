a381a9eabe0c5078dd0039483bd9fd606f73943c
pv2k3 <vermapriyanshu001@gmail.com>
2025-07-07 11:41:49 +0530

    configured port for env

--- app.py
+++ app.py
@@ -98,4 +98,5 @@ async def analyze_location(request: Request, city: str = Form(...),country: str 
         }
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    PORT = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=PORT, reload=True)
